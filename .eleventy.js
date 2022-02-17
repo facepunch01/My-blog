06f513a16944c91b290f189e62fbc3c26c5ef4d5
thatboyjake <jakerhackl@gmail.com>
2022-02-17 16:59:35 -0600

    added navbar

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const htmlmin = require('html-minifier')
+const eleventyGoogleFonts = require("eleventy-google-fonts");
 
 const now = String(Date.now())
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
   eleventyConfig.addWatchTarget('./styles/tailwind.css')
+  eleventyConfig.addPlugin(eleventyGoogleFonts);
 
   eleventyConfig.addPassthroughCopy({
     './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -2,11 +2,14 @@ module.exports = {
   content: ['_site/**/*.html'],
   safelist: [],
   theme: {
+    fontFamily: {
+      'sans': ['Montserrat', 'sans-serif']
+    },
     extend: {
       colors: {
         change: 'black',
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography'), require("daisyui")],
 }
