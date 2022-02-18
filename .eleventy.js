01eb45cd0fb8cb3f87fe1f01eecd218e138ddc76
thatboyjake <jakerhackl@gmail.com>
2022-02-18 17:16:19 -0600

    posts menu

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const htmlmin = require('html-minifier')
 const eleventyGoogleFonts = require("eleventy-google-fonts");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 const now = String(Date.now())
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
   eleventyConfig.addWatchTarget('./styles/tailwind.css')
   eleventyConfig.addPlugin(eleventyGoogleFonts);
@@ -17,6 +19,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     './myjs.js': './js/myjs.js',
   })
+  eleventyConfig.setTemplateFormats(["jpg", "png", "md", "njk"]);
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true,
+    excerpt_separator: "<!-- excerpt -->",
+  });
 
   eleventyConfig.addShortcode('version', function () {
     return now
